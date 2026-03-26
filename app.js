e3dec78a4c62035f25ade9d66f230ef806c11774
Learnovate Developer <developer.learnovate@gmail.com>
2026-03-26 11:36:07 +0530

    Resolve merge conflict in app.js

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 console.log("Server started");
 console.log("Database connected");
 console.log("Login API initialized");
-
+console.log("Feature login validation added");
+console.log("Feature branch change");
+console.log("Main branch improvement");
