18e96d48a7b55409a6c6455972282348f4b64753
Learnovate Developer <developer.learnovate@gmail.com>
2026-03-26 15:42:14 +0530

    Improve database connection log

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 console.log("Server started");
-console.log("Database connected");
+console.log("Database connected successfully");
 console.log("Login API initialized");
 console.log("Feature login validation added");
 console.log("Feature branch change");
