96b8e84aef55575dc96e36e772415dca9571c38a
Learnovate Developer <developer.learnovate@gmail.com>
2026-03-27 07:34:16 +0530

    Update payment API

--- routes.js
+++ routes.js
@@ -3,3 +3,4 @@ console.log("User route initialized");
 console.log("Payment route added");
 console.log("Order route initialized");
 console.log("Orders API integrated");
+console.log("Payment API updated");
